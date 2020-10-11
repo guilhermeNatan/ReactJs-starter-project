180ac3f523fea8c5999c2eff86bd4b25a1b8448b
guilhermeNatan <guilherme.natan.barbosa@gmail.com>
2020-10-11 18:25:41 -0300

    Criando estrutura para reducers e actions

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import {combineReducers} from 'redux';
+
+/**
+ * @type {Reducer<S>}
+ */
+const rootReducer = combineReducers({
+  simplesReducer: 'simplesReducer'
+});
+
+export default rootReducer;
