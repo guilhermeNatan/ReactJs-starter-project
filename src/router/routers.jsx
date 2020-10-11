151dd2dee6adca5b38d9cc07ac4166854f61d734
guilhermeNatan <guilherme.natan.barbosa@gmail.com>
2020-10-11 18:27:38 -0300

    Criando a tela de login

---
+++ src/router/routers.jsx
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import {Provider} from 'react-redux';
+import {applyMiddleware, createStore} from 'redux';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import reduxThunk from 'redux-thunk';
+import {createBrowserHistory} from 'history';
+import {ToastContainer} from 'react-toastify';
+import LayoutInterno from '../layout/pages/LayoutInterno';
+import '../resources/css/style.css';
+import reducers from '../reducers';
+import 'react-toastify/dist/ReactToastify.css';
+import Login from '../layout/pages/login/Login';
+
+const history = createBrowserHistory();
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+
+const renderizarComLayoutPadrao = Componente => props => (
+  <LayoutInterno>
+    <Componente
+      params={props.match.params}
+      router={props}
+      isEditar={props.match.params.operacao === 'editar'}
+      isCriar={props.match.params.operacao === 'criar'}
+      history={history}
+    />
+  </LayoutInterno>
+);
+
+export const renderizarComLayoutLogin = Componente => (props) => {
+  const elemento = (<Componente params={props.match.params} {...props} />);
+  return (
+    <div>
+      {
+          elemento
+        }
+    </div>
+  );
+};
+
+const login = renderizarComLayoutLogin(Login);
+
+class Rotas extends Component {
+  render() {
+    return (
+      <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+          <div>
+            <Switch>
+              <Route exact path="/" component={login} />
+              <Route render={() => <div>Ops : página não encontrada</div>} />
+            </Switch>
+            <ToastContainer />
+          </div>
+        </BrowserRouter>
+
+      </Provider>
+    );
+  }
+}
+
+
+export default Rotas;
+
