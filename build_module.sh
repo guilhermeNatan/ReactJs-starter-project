71e9e52b3ea029e1758f46904f509bef72df9bc8
guilhermeNatan <guilherme.natan.barbosa@gmail.com>
2020-10-11 23:19:36 -0300

    Adcionando scripts para automatizar a criação de páginas

--- build_module.sh
+++ build_module.sh
@@ -10,7 +10,7 @@ mkdir  -p src/layout/pages/$NAME/components
 sed "s/TemplateScreen/${NAME}Screen/g" src/TemplateModule/TemplateScreen.js >  src/layout/pages/$NAME/${NAME}Screen.js
 sed "s/TemplateScreen/${NAME}Screen/g" src/TemplateModule/index.js >  src/layout/pages/$NAME/index.js
 cp src/TemplateModule/style.js   src/layout/pages/$NAME
-
+mv  src/layout/pages/$NAME/style.js src/layout/pages/$NAME/${NAME}Styles.js
 
 
 
