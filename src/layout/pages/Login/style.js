657e06b1d334996cdb275213aa3c21ed0b094f00
guilhermeNatan <guilherme.natan.barbosa@gmail.com>
2020-10-11 22:57:14 -0300

    Renomeando a página de login

---
+++ src/layout/pages/Login/style.js
@@ -0,0 +1,17 @@
+import {palette} from "../../colors/Colors";
+
+export default {
+    container: {
+        display: 'flex',
+        justifyContent: 'center',
+        paddingTop: '5%',
+        backgroundColor: palette.secondaryDarkColor,
+        height: '100vh',
+        width: '100%',
+        alignItems: 'center',
+
+    },
+    card: {
+        width: '50%',
+    },
+};
