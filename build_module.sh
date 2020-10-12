18ca3af9a16b02b4f7ee4d8cf6506da180b53071
guilhermeNatan <guilherme.natan.barbosa@gmail.com>
2020-10-11 23:00:50 -0300

    Adcionando scripts para automatizar a criação de páginas automaticamente

---
+++ build_module.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# @author guilherme.natan
+#Script usado para criar telas respeitando a estrutura de diretorios do projeto
+
+
+echo -n "Entre com o nome do modulo <ex Settings>: "
+read NAME
+mkdir  -p src/layout/pages/$NAME/components
+sed "s/TemplateScreen/${NAME}Screen/g" src/TemplateModule/TemplateScreen.js >  src/layout/pages/$NAME/${NAME}Screen.js
+sed "s/TemplateScreen/${NAME}Screen/g" src/TemplateModule/index.js >  src/layout/pages/$NAME/index.js
+cp src/TemplateModule/style.js   src/layout/pages/$NAME
+
+
+
+
+
